04b3684fbbb15a05e5b847451a3eb53b49019422
josh-hemphill <j1.robofan@gmail.com>
2026-03-04 20:18:05 -0800

    style: format test config

--- packages/workspace/vitest.config.ts
+++ packages/workspace/vitest.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig, type TestUserConfig } from 'vitest/config';
+import type { TestUserConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
