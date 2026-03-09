e5d5a11b94ab3382d1b1e6c2a121e8bd850b23ce
josh-hemphill <j1.robofan@gmail.com>
2026-03-08 23:34:11 -0700

    fix(release): allowDirty does not depend on publishType [skip ci]

--- packages/release/src/executor.ts
+++ packages/release/src/executor.ts
@@ -64,14 +64,14 @@ export async function executePublish(
 	logger.trace('Contextualized publish command', { command });
 
 	if (allowDirty) {
-		if (config.publishType === 'npm' && config.command.name === 'pnpm') {
+		if (config.command.name === 'pnpm') {
 			command.args.push('--no-git-checks');
 		}
-		else if (config.publishType === 'jsr' && config.command.name === 'deno') {
+		else if (config.command.name === 'deno') {
 			command.args.push('--allow-dirty');
 		}
 		else {
-			throw new Error(`Allow dirty is not supported for ${config.publishType} ${config.command.name}`);
+			throw new Error(`Allow dirty is not supported for ${config.command.name}`);
 		}
 	}
 
